14924d8cc3d2485b8705ba407fd39964bcc23b05
Joshua Correa <jdcorrea17@gmail.com>
2022-07-28 16:29:18 -0500

    menu button added

---
+++ server/controller.js
@@ -0,0 +1,6 @@
+module.exports = {
+    getReview: (req, res) => {
+        const {Email, Review, Rating, date} = req.params
+        const newReview = ()
+    }
+}
--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ const path = require('path')
 
 const app = express()
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd1d0cbb3076845a39097a3edca27e7a1',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const {getReview} = requ
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
