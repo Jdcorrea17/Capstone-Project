d00b2d97681618a0c17b2213010ddc2258b85596
Joshua Correa <jdcorrea17@gmail.com>
2022-07-28 16:49:53 -0500

    error fixed

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,6 @@
-module.exports = {
-    getReview: (req, res) => {
-        const {Email, Review, Rating, date} = req.params
-        const newReview = ()
-    }
-}
+// module.exports = {
+//     getReview: (req, res) => {
+//         const {Email, Review, Rating, date} = req.params
+//         const newReview = ()
+//     }
+// }
